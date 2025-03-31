be83a0e8ac5ff2c1499e763612a0b1173603eaf0
Natnael707 <natnaelalemtsge@gmail.com>
2025-03-31 22:54:42 +0300

    edited the identifier name to mysql_db_ triggered_by_Jenkins

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "mydb" {
   engine              = "mysql"
   engine_version      = "8.0"
   instance_class      = "db.t3.micro"
-  identifier          = "my-mysql-db"
+  identifier          = "mysql_db_ triggered_by_Jenkins"
 username = var.mysql_db_username
 password = var.mysql_db_password
   parameter_group_name = "default.mysql8.0"
